c39555bd949488f6db3eeda0587fe418eada74f1
Atipat <atipatmpsnb79@gmail.com>
2024-08-30 15:54:45 +0700

    end week 7

---
+++ ep7#308/ep.7.ts
@@ -0,0 +1,69 @@
+// const score : number[] = [10,23,26,30,33] 
+
+// let sum: number = 0
+
+// for(let i = 0; i < score.length; i++) {
+//     sum = sum + score[i]
+// }
+
+// let avg = sum /score.length
+
+// console.log ("total:  ", sum)
+// console.log("avg", avg)
+// // ทบทวน
+
+// const myStudents: string = 'เซน,แทน,เวฟ,รุ้ง,แท็ค,สร'
+
+// const myStudentsArry: string[] = myStudents.split(',')
+
+// console.log(myStudents)
+// console.log(myStudentsArry[1])
+
+// const myAnimal: string = 'แมว*นกฮูก*เต่า*มังกร'
+
+// const myAnimalArry: string[] = myAnimal.split('*')
+
+// console.log(myAnimal)
+// console.log(myAnimalArry)
+
+// const myAnimal: string = 'แมว*นกฮูก*เต่า*มังกร'
+
+// const myAnimalArry: string[] = myAnimal.split('*')
+
+// myAnimalArry.pop()
+
+// console.log("ตั้งแต่: ",myAnimalArry)
+
+// myAnimalArry.push('สิงโต')
+
+// console.log("ใส่ต่อ: ",myAnimalArry)
+
+// myAnimalArry.shift()
+
+// console.log("เอาตัวหน้าออก: ",myAnimalArry)
+
+// myAnimalArry.unshift('แมวเปอร์เซีย')
+
+// console.log("เอาตัวหน้าออก: ",myAnimalArry)
+
+// const fruits: string = ('cat','owl','tao','Dragon')
+
+// a
+
+// console.log("เรียงA-Z",fruits)
+
+// fruits.reverse()
+
+// console.log("เรียงZ-A",fruits)
+
+// const nickName: string = ('punn,time,forth,flim')
+// const nickNameArry = nickName.split(',')
+
+// nickNameArry.sort()
+
+// console.log(nickNameArry)
+
+// nickNameArry.reverse()
+
+// console.log(nickNameArry)
+
---
+++ ep7#308/homework.ts
@@ -0,0 +1,6 @@
+const me = {
+    frstName: 'wolverine',
+    lastName: 'deadpool',
+    age:785 ,
+    numberPhone: '0001111234'
+}
